a748c818b43ce110344f54f2f777cec1862e2178
PhamDuy1810204 <phamdinhanhduy@gmail.com>
2024-08-04 21:34:31 +0700

    Initial Project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className='h-screen flex justify-center items-center'>
+        <span className='font-bold text-red-500 text-xl'>Hello</span>
+      </div>
+    </>
+  )
+}
+
+export default App
