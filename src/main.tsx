bc3d70288ea74681a24ba1642d07d1537e212f69
Nguyen Trinh Vinh Thinh <thinhss123321@gmail.com>
2024-08-11 22:50:56 +0700

    Feat: create forgotpassword page

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,12 @@ import store from './Redux/store.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import LoginPage from './pages/loginPage.tsx'
 import RegisterPage from './pages/registerPage.tsx'
+import VerifiedEmail from './pages/verifiedEmail.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <React.StrictMode>
       <Provider store={store}>
-        <RegisterPage />
+        <VerifiedEmail />
       </Provider>
     </React.StrictMode>
   </BrowserRouter>
